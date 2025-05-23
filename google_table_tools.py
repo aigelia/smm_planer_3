7d6e4edd9a5010c3d7504622c95d6fc84d2a8b69
EvgenyGorG <evgenygorokhovg@yahoo.com>
2025-05-23 15:15:36 +0300

    Added the ability to get data from google sheet

---
+++ google_table_tools.py
@@ -0,0 +1,12 @@
+from gspread import service_account
+from pathlib import Path
+
+
+def get_google_table_records(path_to_google_credentials_file, google_table_url):
+        client_info = Path(path_to_google_credentials_file)
+        client = service_account(filename=client_info)
+
+        table = client.open_by_url(google_table_url)
+        sheet = table.sheet1
+
+        return sheet.get_all_records()
