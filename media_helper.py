f6382086cb6af31806e0564689312ede57e02f48
Katerina <e.mayakovskaya@gmail.com>
2025-05-24 17:48:34 +0300

    Add module for downloading mediacontent

---
+++ media_helper.py
@@ -0,0 +1,66 @@
+import os
+import mimetypes
+import re
+
+import requests
+
+
+def extract_drive_url(media_url):
+    """Извлекает ID файла из ссылки на Google Drive."""
+    patterns = [
+        r'drive\.google\.com\/fiйle\/d\/([^\/\?]+)',
+        r'drive\.google\.com\/open\?id=([^&]+)',
+        r'drive\.google\.com\/uc\?id=([^&]+)'
+    ]
+    for pattern in patterns:
+        match = re.search(pattern, media_url)
+        if match:
+            return match.group(1)
+    raise ValueError("ID файла Google Drive не найдено. Проверьте настройки доступа.")
+
+
+def download_file(download_link):
+    """Скачивает файл по ссылке и сохраняет как временный файл в директорию проекта."""
+    headers = {
+        "User-Agent": "Mozilla/5.0"
+    }
+
+    response = requests.get(download_link, headers=headers)
+    response.raise_for_status()
+
+    content_type = response.headers.get("Content-Type", "").split(";")[0]
+    extension = mimetypes.guess_extension(content_type)
+
+    filename = f"temp_download{extension}"
+    save_path = os.path.join(os.getcwd(), filename)
+
+    with open(save_path, 'wb') as f:
+        f.write(response.content)
+
+    return save_path, content_type
+
+
+def get_image(media_url):
+    """Определяет источник изображения, сохраняет файл и возвращает его тип и путь"""
+    if 'drive.google.com' in media_url:
+        file_id = extract_drive_url(media_url)
+        download_link = f'https://drive.google.com/uc?export=download&id={file_id}'
+    else:
+        download_link = media_url
+
+    save_path, content_type = download_file(download_link)
+
+    if content_type == 'image/gif':
+        media_type = 'gif'
+    elif content_type.startswith('image/'):
+        media_type = 'image'
+    else:
+        media_type = 'unknown'
+
+    return save_path, media_type
+
+
+if __name__ == '__main__':
+    image_path, image_type = get_image('https://www.investopedia.com/thmb/PuY9jSC3O7hx65zubwf4cJV5TPw=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/GettyImages-2149536121-26e918a317de4f618433b579f15135f8.jpg')
+    print(f"Файл сохранён: {image_path}")
+    print(f"Тип файла: {image_type}")
