6bd4744e6ca056297a2aa56e987092483ae8c4a5
EvgenyGorG <evgenygorokhovg@yahoo.com>
2025-05-24 19:33:07 +0300

    Added notes

--- google_table_tools.py
+++ google_table_tools.py
@@ -1,5 +1,7 @@
-from gspread import service_account
 from pathlib import Path
+
+import requests
+from gspread import service_account
 
 
 def get_google_table_records(path_to_google_credentials_file, google_table_url):
@@ -12,4 +14,13 @@ def get_google_table_records(path_to_google_credentials_file, google_table_url):
         return sheet.get_all_records()
 
 
+def get_text_from_google_doc(google_doc_url):
+        google_doc = requests.get(google_doc_url)
+        google_doc.raise_for_status()
+        return google_doc.text
+
+
+
 # TODO: Предоставить пользователю credentials?
+
+print(get_text_from_google_doc('https://docs.google.com/document/d/16Jhi7SWlCa2Ptpl6NCVy8YFRFOz3zMFPclh3Gcjq9iI/export?format=txt'))
