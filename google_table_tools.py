6c187140b35fb9178ded7438b26263acb49e40fd
Evgeny <evgenygorokhovg@yahoo.com>
2025-05-25 16:51:34 +0300

    Added the ability to enter data into a table

--- google_table_tools.py
+++ google_table_tools.py
@@ -14,13 +14,14 @@ def get_google_table_records(path_to_google_credentials_file, google_table_url):
         return sheet.get_all_records()
 
 
-def get_text_from_google_doc(google_doc_url):
-        google_doc = requests.get(google_doc_url)
-        google_doc.raise_for_status()
-        return google_doc.text
+def write_data_in_table_cell(path_to_google_credentials_file, google_table_url, post_id, cell_letter):
+        client_info = Path(path_to_google_credentials_file)
+        client = service_account(filename=client_info)
+
+        table = client.open_by_url(google_table_url)
+        sheet = table.sheet1
+
+        sheet.update(f'{cell_letter}{str(post_id)}', 'Опубликовано')
 
 
-
-# TODO: Предоставить пользователю credentials?
-
-print(get_text_from_google_doc('https://docs.google.com/document/d/16Jhi7SWlCa2Ptpl6NCVy8YFRFOz3zMFPclh3Gcjq9iI/export?format=txt'))
+# # TODO: Предоставить пользователю credentials?
