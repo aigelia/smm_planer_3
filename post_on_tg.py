3b0a4477ebe7ddd328077ad63b99de1e9eec9b68
Katerina <e.mayakovskaya@gmail.com>
2025-05-25 23:03:41 +0300

    Fix Telegram module

--- post_on_tg.py
+++ post_on_tg.py
@@ -2,16 +2,17 @@ import os
 from dotenv import load_dotenv
 import telegram
 
-
 def publish_on_telegram(channel_id, post_text, media_path=None):
     load_dotenv()
     tg_token = os.environ['TG_TOKEN']
     bot = telegram.Bot(token=tg_token)
 
-    if media_path:
-        if media_path.endswith('.gif'):
-            bot.send_animation(chat_id=channel_id, animation=media_path, caption=post_text)
-        else:
-            bot.send_photo(chat_id=channel_id, photo=media_path, caption=post_text)
+    if media_path and os.path.exists(media_path):
+        mode = 'animation' if media_path.lower().endswith('.gif') else 'photo'
+        with open(media_path, 'rb') as f:
+            if mode == 'animation':
+                bot.send_animation(chat_id=channel_id, animation=f, caption=post_text)
+            else:
+                bot.send_photo(chat_id=channel_id, photo=f, caption=post_text)
     else:
         bot.send_message(chat_id=channel_id, text=post_text)
