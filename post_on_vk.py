29df8faf49f843566278a6f7204f61210e329eb9
Evgeny <evgenygorokhovg@yahoo.com>
2025-05-26 14:38:49 +0300

    Redesigned the code structure

--- post_on_vk.py
+++ post_on_vk.py
@@ -1,14 +1,20 @@
+import os
+
 import requests
+from dotenv import load_dotenv
 
 
-def get_image_upload_url(vk_access_token, vk_page_id):
+load_dotenv()
+VK_ACCESS_TOKEN = os.environ['VK_ACCESS_TOKEN']
+
+
+def get_image_upload_url(vk_page_id):
     """Получает url для загрузки изображения на сервер."""
-
     vk_api_url = 'https://api.vk.com/method/photos.getWallUploadServer'
 
     query_parameters = {
         'group_id': vk_page_id,
-        'access_token': vk_access_token,
+        'access_token': VK_ACCESS_TOKEN,
         'v': '5.199'
     }
 
@@ -20,7 +26,6 @@ def get_image_upload_url(vk_access_token, vk_page_id):
 
 def upload_image(vk_image_path, image_upload_url):
     """Загружает изображение на сервер."""
-
     with open(vk_image_path, 'rb') as image:
         image_params = {
             'photo': image
@@ -32,14 +37,13 @@ def upload_image(vk_image_path, image_upload_url):
         return uploaded_image.json()
 
 
-def save_image_on_wall(vk_access_token, vk_page_id, uploaded_image):
+def save_image_on_wall(vk_page_id, uploaded_image):
     """Сохраняет изображение на стене."""
-
     vk_api_url = 'https://api.vk.com/method/photos.saveWallPhoto'
 
     query_params = {
         'group_id': vk_page_id,
-        'access_token': vk_access_token,
+        'access_token': VK_ACCESS_TOKEN,
         'v': '5.199',
         'server': uploaded_image['server'],
         'photo': uploaded_image['photo'],
@@ -52,27 +56,24 @@ def save_image_on_wall(vk_access_token, vk_page_id, uploaded_image):
     return saved_image.json()
 
 
-def get_gif_upload_url(vk_access_token, vk_page_id):
+def get_gif_upload_url(vk_page_id):
     """Получает url для загрузки гифки на сервер."""
-
     vk_api_url = 'https://api.vk.com/method/docs.getWallUploadServer'
 
     query_params = {
         'group_id': vk_page_id,
-        'access_token': vk_access_token,
+        'access_token': VK_ACCESS_TOKEN,
         'v': '5.199'
     }
 
     gif_upload_url = requests.get(vk_api_url, params=query_params)
     gif_upload_url.raise_for_status()
-    print(gif_upload_url.json())
 
     return gif_upload_url.json()['response']['upload_url']
 
 
-def upload_gif(gif_upload_url, vk_gif_path):
+def upload_gif(vk_gif_path, gif_upload_url):
     """Загружает гифку на сервер."""
-
     with open(vk_gif_path, 'rb') as gif:
         query_params = {'file': gif}
 
@@ -82,12 +83,11 @@ def upload_gif(gif_upload_url, vk_gif_path):
         return uploaded_gif.json()['file']
 
 
-def save_doc(vk_access_token, uploaded_gif):
+def save_doc(uploaded_gif):
     """Сохраняет гифку для последующего постинга."""
-
     query_params = {
         'file': uploaded_gif,
-        'access_token': vk_access_token,
+        'access_token': VK_ACCESS_TOKEN,
         'v': '5.199'
     }
 
@@ -97,9 +97,8 @@ def save_doc(vk_access_token, uploaded_gif):
     return saved_doc.json()['response']['doc']
 
 
-def publish_post_in_vk(vk_access_token, vk_page_id, media_type, saved_file=None, text=''):
+def publish_post_in_vk(vk_page_id, media_type=None, saved_file=None, text=''):
     """Публикует контент на стене."""
-
     vk_api_url = 'https://api.vk.com/method/wall.post'
 
     if saved_file:
@@ -116,14 +115,14 @@ def publish_post_in_vk(vk_access_token, vk_page_id, media_type, saved_file=None,
             'owner_id': f'-{vk_page_id}',
             'message': text,
             'attachments': attachments,
-            'access_token': vk_access_token,
+            'access_token': VK_ACCESS_TOKEN,
             'v': '5.199'
         }
     else:
         query_parameters = {
             'owner_id': f'-{vk_page_id}',
             'message': text,
-            'access_token': vk_access_token,
+            'access_token': VK_ACCESS_TOKEN,
             'v': '5.199'
         }
 
